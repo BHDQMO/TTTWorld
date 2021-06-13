0d68de3eb42cfa0a5a9311d8678ddb51c53491b4
BHDQMO <74060604+BHDQMO@users.noreply.github.com>
2021-06-14 02:02:39 +0800

    add ice STUN & TURN server when create RTCPeerConnection()

--- public/src/friend.js
+++ public/src/friend.js
@@ -1085,7 +1085,23 @@ function closing() {
 
 // utils
 async function createPeerConnection() {
-  localPeer = new RTCPeerConnection()
+  localPeer = new RTCPeerConnection({
+    'iceServers': [
+      {
+        'urls': 'stun:stun.l.google.com:19302'
+      },
+      {
+        'urls': 'turn:192.158.29.39:3478?transport=udp',
+        'credential': 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
+        'username': '28224511:1379330808'
+      },
+      {
+        'urls': 'turn:192.158.29.39:3478?transport=tcp',
+        'credential': 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
+        'username': '28224511:1379330808'
+      }
+    ]
+  })
   localPeer.onicecandidate = handleIceCandidate
   localPeer.onnegotiationneeded = handleNegotiationNeeded
   localPeer.ontrack = handleRemoteStream
